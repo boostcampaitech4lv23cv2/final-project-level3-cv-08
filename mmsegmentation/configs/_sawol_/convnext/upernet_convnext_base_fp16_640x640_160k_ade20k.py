05ed5a1c98ba4226436082e7c3aa524108084001
22eming <gurrl96@gmail.com>
2023-01-16 00:48:24 +0000

    change 512 -> 640 image size

--- mmsegmentation/configs/_sawol_/convnext/upernet_convnext_base_fp16_640x640_160k_ade20k.py
+++ mmsegmentation/configs/_sawol_/convnext/upernet_convnext_base_fp16_640x640_160k_ade20k.py
@@ -1,7 +1,7 @@
 _base_ = [
-    '../_base_/models/upernet_convnext.py',
-    '../_base_/datasets/ade20k_640x640.py', '../_base_/default_runtime.py',
-    '../_base_/schedules/schedule_160k.py'
+    './models/upernet_convnext.py',
+    './datasets/ade20k_640x640.py', './default_runtime.py',
+    './schedules/schedule_160k.py'
 ]
 crop_size = (640, 640)
 checkpoint_file = 'https://download.openmmlab.com/mmclassification/v0/convnext/downstream/convnext-base_3rdparty_in21k_20220301-262fd037.pth'  # noqa
@@ -18,37 +18,38 @@ model = dict(
             prefix='backbone.')),
     decode_head=dict(
         in_channels=[128, 256, 512, 1024],
-        num_classes=150,
+        num_classes=2,
     ),
-    auxiliary_head=dict(in_channels=512, num_classes=150),
+    auxiliary_head=dict(in_channels=512, num_classes=2),
     test_cfg=dict(mode='slide', crop_size=crop_size, stride=(426, 426)),
 )
 
 optimizer = dict(
-    constructor='LearningRateDecayOptimizerConstructor',
     _delete_=True,
     type='AdamW',
     lr=0.0001,
     betas=(0.9, 0.999),
     weight_decay=0.05,
-    paramwise_cfg={
-        'decay_rate': 0.9,
-        'decay_type': 'stage_wise',
-        'num_layers': 12
-    })
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.)
+        }))
 
 lr_config = dict(
     _delete_=True,
-    policy='poly',
+    policy='CosineRestart',
     warmup='linear',
-    warmup_iters=1500,
+    warmup_iters=1000,
     warmup_ratio=1e-6,
-    power=1.0,
-    min_lr=0.0,
+    min_lr_ratio=1e-6,
+    periods=[20000, 40000, 60000, 80000],
+    restart_weights=[1.0, 0.6, 0.4, 0.2],
     by_epoch=False)
 
 # By default, models are trained on 8 GPUs with 2 images per GPU
-data = dict(samples_per_gpu=2)
+data = dict(samples_per_gpu=8)
 # fp16 settings
 optimizer_config = dict(type='Fp16OptimizerHook', loss_scale='dynamic')
 # fp16 placeholder
