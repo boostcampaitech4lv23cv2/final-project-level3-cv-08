36473220ce09806a5f8bca5e1237f9eb9e49bc16
22eming <gurrl96@gmail.com>
2023-02-02 16:28:02 +0000

    업데이트: 이미지 저장 plt -> cv2

--- mmsegmentation/app/backend.py
+++ mmsegmentation/app/backend.py
@@ -1,8 +1,7 @@
 import io
 import sys
 import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
+import cv2
 
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import FileResponse
@@ -33,20 +32,22 @@ models = config['models']
 async def make_pred(files: List[UploadFile] = File(...)):
     for file in files:
         image_info = ImageId()
-        image_bytes = await file.read()
-        image = Image.open(io.BytesIO(image_bytes))
-        image = image.convert("RGB")
-        image_array = np.array(image)
-
         id = str(image_info.id)
         image_path = SAVE_PATH + id + ".jpg"
-        plt.imsave(image_path, image_array)
-        image_info.img_url = upload_image(id + ".jpg")
         pred_path = SAVE_PATH + id + "_pred.png"
+        
+        image_bytes = await file.read()       
+        img_array = np.frombuffer(image_bytes, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_UNCHANGED)
+        cv2.imwrite(image_path, img)
+        
+        image_info.img_url = upload_image(id + ".jpg")
         
         for model_key, model_val in models.items():
             pred_image = predict_image(model_val, image_path)
-            plt.imsave(pred_path, pred_image[0])
+            pred_image = np.array(pred_image[0])
+            img[pred_image == 1] = [0,0,0]
+            cv2.imwrite(pred_path, img)
             image_info.pred_url = upload_image(id + "_pred.png")
             image_info.damage = damage_info[damage_check(pred_image)]
             return image_info
